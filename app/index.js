dd8a2d57ee06065833686bab93084036a775a6f5
vsumit698 <50446660+vsumit698@users.noreply.github.com>
2021-02-23 14:20:24 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -4,4 +4,4 @@ import './app.global.css';
 import {BrowserRouter} from 'react-router-dom';
 import App from './AppContainer/app';
 
-ReactDOM.render(<BrowserRouter ><App /></BrowserRouter>,document.getElementById('root'));
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,document.getElementById('root'));
